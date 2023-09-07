460345b9f84eb7dc359f05f4a3ad126dbb8b3557
Harkirats043 <Harkirats043@gmail.com>
2023-09-07 16:05:50 -0400

    Login Auth

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,37 +1,39 @@
 import { Link } from "react-router-dom";
- import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from "./Auth/LoginButton";
 import LogoutButton from "./Auth/LogoutButton";
 
 export default function Homepage() {
-     const {isLoading,isAuthenticated} = useAuth0()
+    const { isLoading, isAuthenticated } = useAuth0();
+
+    if (isLoading) {
+        return <div>Loading...</div>; // Show a loading indicator while authentication state is being determined.
+    }
 
     return (
-
         <div className="text-center">
             <Link to="/">
                 <div className="bg-gradient-to-r from-indigo-500 via-purple-400 to-pink-300 w-full h-15 my-2 font-mono font-extrabold text-[56px] max-md ">Homepage</div>
             </Link>
 
-            {  !isLoading ? (<><div>
+            {isAuthenticated ? (
+                <>
+                    <div>
+                        <LogoutButton />
+                    </div>
 
-
-                {isAuthenticated? <LogoutButton />
-                 : 
-                <LoginButton />}
-
-
-            </div></>) : null  }
-
-            
-            <div className="sm:flex sm:justify-center">
-            <Link to="/FoxPictures">
-                <div className="absolute top-25 left-20 max-sm:top-15 max-sm:left-2 text-[24px] bg-blue-500 hover:bg-blue-400 text-white py-1 px-2 border-b-4 border-blue-700 hover:border-blue-500 rounded"> Fox Images</div>
-            </Link>
-            <Link to="/DogsDetails">
-                <div className="absolute top-30 right-20 max-sm:top-15 max-sm:right-2 text-[24px]  bg-blue-500 hover:bg-blue-400 text-white py-1 px-2 border-b-4 border-blue-700 hover:border-blue-500 rounded">Dog Images</div>
-            </Link>
+                    <div className="sm:flex sm:justify-center">
+                        <Link to="/FoxPictures">
+                            <div className="absolute top-25 left-20 max-sm:top-15 max-sm:left-2 text-[24px] bg-blue-500 hover:bg-blue-400 text-white py-1 px-2 border-b-4 border-blue-700 hover:border-blue-500 rounded"> Fox Images</div>
+                        </Link>
+                        <Link to="/DogsDetails">
+                            <div className="absolute top-30 right-20 max-sm:top-15 max-sm:right-2 text-[24px]  bg-blue-500 hover:bg-blue-400 text-white py-1 px-2 border-b-4 border-blue-700 hover:border-blue-500 rounded">Dog Images</div>
+                        </Link>
+                    </div>
+                </>
+            ) : (
+                <LoginButton />
+            )}
         </div>
-        </div>
-    )
+    );
 }
