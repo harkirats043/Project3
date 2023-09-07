8e4dadd2913a5c92d0092f4f12c7eb81f2166c54
Harkirats043 <Harkirats043@gmail.com>
2023-09-07 15:44:43 -0400

    update login page

--- src/components/Auth/AuthProvider.jsx
+++ src/components/Auth/AuthProvider.jsx
@@ -7,7 +7,8 @@ export default function Auth0ProviderWithNavigation({children}) {
     const domain = process.env.REACT_APP_AUTH0_DOMAIN
     const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
     const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URI
-
+ console.log(domain,clientId,redirectUri);
+ 
     if (!(domain && clientId && redirectUri)) {
         return null
     }
