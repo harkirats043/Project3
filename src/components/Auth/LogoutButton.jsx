8c12ad5c867a03626e07f5c1c758be70e297008b
Harkirats043 <Harkirats043@gmail.com>
2023-09-08 09:35:09 -0400

    udpate login/logout

--- src/components/Auth/LogoutButton.jsx
+++ src/components/Auth/LogoutButton.jsx
@@ -3,6 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 export default function LogoutButton(){
     const{ logout } = useAuth0()
 
-    return (<button onClick={()=>logout()}>Log Out</button>)
+    return (<button className="bg-blue-500 hover:bg-blue-800 text-white font-bold py-1.5 px-4 rounded-full"
+        onClick={()=>logout()}>Log Out</button>)
 
 }
